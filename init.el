50f9fb67b67e27c885a2ee665cf96d54d72d4518
qzi <i@qzier.com>
2013-03-20 05:09:14 +0800

    clear up the init.el

--- init.el
+++ init.el
@@ -1,4 +1,3 @@
-
 ;; author: qzi
 ;; email: i@qzier.com
 ;; content: emacs setting
@@ -9,145 +8,20 @@
 (add-to-list 'load-path (expand-file-name "~/.emacs.d"))
 
 ;; require
-(require 'init-repo)
+(require 'init-repo) ;; loadpath
 (require 'init-env)
 (require 'init-ui)
-(require 'init-yas-ac)
 (require 'init-utils)
+(require 'init-yas-ac)
 (require 'init-lisp)
 (require 'init-html)
+(require 'init-css)
 (require 'init-js)
 (require 'init-markdown)
 (require 'init-tex)
-(require 'init-keymap) ;; key map
+(require 'init-c)  ;; after init-yas-ac
+(require 'init-keymap) ;; keyboard map
 (require 'init-postfix) ;; 放到大多数库的后面
-
-
-;; 以下待验证
-
-(require 'cedet)
-(global-ede-mode t)
-;;(semantic-mode)
-;;(semantic-load-enable-minimum-features)
-;;(semantic-load-enable-code-helpers)
-;;(semantic-load-enable-guady-code-helpers)
-;;(semantic-load-enable-excessive-code-helpers)
-;;(semantic-load-enable-semantic-debugging-helpers)
-(setq semantic-default-submodes '(global-semantic-idle-scheduler-mode
-				  global-semanticdb-minor-mode
-				  global-semantic-idle-summary-mode
-				  global-semantic-mru-bookmark-mode))
-(semantic-mode t)
-(global-semantic-highlight-edits-mode (if window-system 1 -1))
-(global-semantic-show-unmatched-syntax-mode 1)
-(global-semantic-show-parser-state-mode 1)
-
-
-
-;; Semantic/ia
-;; smart complitions
-(require 'semantic/ia)
-(setq-mode-local c-mode semanticdb-find-default-throttle
-                 '(project unloaded system recursive))
-(setq-mode-local c++-mode semanticdb-find-default-throttle
-                 '(project unloaded system recursive))
-
-;; header
-;;;; Include settings
-(require 'semantic/bovine/gcc)
-(require 'semantic/bovine/c)
-
-(defconst cedet-user-include-dirs
-  (list ".." "../include" "../inc" "../common" "../public" "."
-        "../.." "../../include" "../../inc" "../../common" "../../public"))
-
-(setq cedet-sys-include-dirs (list
-                              "/usr/include"
-			      "/usr/include/gcc/darwin/4.2"
-			      "/usr/include/c++/4.2.1"
-                              "/usr/local/include"))
-
-(let ((include-dirs cedet-user-include-dirs))
-  (setq include-dirs (append include-dirs cedet-sys-include-dirs))
-  (mapc (lambda (dir)
-          (semantic-add-system-include dir 'c++-mode)
-          (semantic-add-system-include dir 'c-mode))
-        include-dirs))
-
-(setq semantic-c-dependency-system-include-path "/usr/include/")
-
-;; semantic 通过imenu集成到emacs
-;;;; TAGS Menu
-(defun my-semantic-hook ()
-  (imenu-add-to-menubar "TAGS"))
-
-(add-hook 'semantic-init-hooks 'my-semantic-hook)
-
-;;;; Semantic DataBase存储位置
-(setq semanticdb-default-save-directory
-      (expand-file-name "~/.emacs.d/semanticdb"))
-
-;; 使用 gnu global 的TAGS。
-(require 'semantic/db-global)
-(semanticdb-enable-gnu-global-databases 'c-mode)
-(semanticdb-enable-gnu-global-databases 'c++-mode)
-
-;;;;  缩进或者补齐
-;;; hippie-try-expand settings
-(setq hippie-expand-try-functions-list
-      '(
-        yas/hippie-try-expand
-        semantic-ia-complete-symbol
-        try-expand-dabbrev
-        try-expand-dabbrev-visible
-        try-expand-dabbrev-all-buffers
-        try-expand-dabbrev-from-kill
-        try-complete-file-name-partially
-        try-complete-file-name
-        try-expand-all-abbrevs))
-
-(defun indent-or-complete ()
-  "Complete if point is at end of a word, otherwise indent line."
-  (interactive)
-  (if (looking-at "\\>")
-      (hippie-expand nil)
-    (indent-for-tab-command)
-    ))
-
-(defun yyc/indent-key-setup ()
-  "Set tab as key for indent-or-complete"
-  (local-set-key  [(tab)] 'indent-or-complete)
-  )
-
-;;;; C-mode-hooks .
-(defun yyc/c-mode-keys ()
-  "description"
-  ;; Semantic functions.
-  (semantic-default-c-setup)
-  (local-set-key "\C-c?" 'semantic-ia-complete-symbol-menu)
-  (local-set-key "\C-cb" 'semantic-mrub-switch-tags)
-  (local-set-key "\C-cR" 'semantic-symref)
-  (local-set-key "\C-cj" 'semantic-ia-fast-jump)
-  (local-set-key "\C-cp" 'semantic-ia-show-summary)
-  (local-set-key "\C-cl" 'semantic-ia-show-doc)
-  (local-set-key "\C-cr" 'semantic-symref-symbol)
-  (local-set-key "\C-c/" 'semantic-ia-complete-symbol)
-  (local-set-key [(control return)] 'semantic-ia-complete-symbol)
-  (local-set-key "." 'semantic-complete-self-insert)
-  (local-set-key ">" 'semantic-complete-self-insert)
-  ;; Indent or complete
-  (local-set-key  [(tab)] 'indent-or-complete)
-  )
-(add-hook 'c-mode-common-hook 'yyc/c-mode-keys)
-
-(defadvice push-mark (around semantic-mru-bookmark activate)
-  "Push a mark at LOCATION with NOMSG and ACTIVATE passed to `push-mark'.
-If `semantic-mru-bookmark-mode' is active, also push a tag onto
-the mru bookmark stack."
-  (semantic-mrub-push semantic-mru-bookmark-ring
-                      (point)
-                      'mark)
-  ad-do-it)
 
 
 ;;; 下面功能正在验证中  ...
@@ -178,40 +52,8 @@ the mru bookmark stack."
 
 
 ;; anything
-(add-to-list 'load-path "~/.emacs.d/plugins/helm")
-(require 'helm-config)
+;;(add-to-list 'load-path "~/.emacs.d/plugins/helm")
+;;(require 'helm-config)
 ;;(helm-mode 1)
-(global-set-key (kbd "C-c h") 'helm-mini)
+;;(global-set-key (kbd "C-c h") 'helm-mini)
 
-
-;;定义在cpp文件和.h文件中切换的函数
-(defun switch-source-file ()
-  (interactive)
-  (setq file-name (buffer-file-name))
-  (if (string-match "\\.c" file-name)
-      (find-file (replace-regexp-in-string "\\.c" "\.h" file-name)))
-  (if (string-match "\\.h" file-name)
-      (find-file (replace-regexp-in-string "\\.h" "\.c" file-name))))
-
-(global-set-key [f12] 'switch-source-file)
-
-
-;; 输入 inc , 可以自动提示输入文件名称,可以自动补全.
-;;(define-skeleton skeleton-include
-;;  "generate include""" ""
-;;  > "#include \""
-;;  (completing-read "Include File:"
-;;                   (mapcar #'(lambda (f) (list f ))
-;;                           (apply 'append
-;;                                  (mapcar
-;;                                   #'(lambda (dir)
-;;                                       (directory-files dir))
-;;                                   (list "/usr/include"
-;; 					 "./"
-;;                                         "/usr/local/include"
-;;                                         "/usr/include/c++/4.2.4"
-;; 					 "/usr/include/c++/4.1"
-;; 					 "/usr/include/c++/4.1.3"
-;; 					 "/usr/include/c++/4.2"
-;; 					 )))))
-;;"\"")

ae02481fcd27a36c847f3311fdbf16db648ceff6
qzi <i@qzier.com>
2013-03-20 04:58:45 +0800

    auctex

--- init-tex.el
+++ init-tex.el
@@ -2,8 +2,33 @@
 ;; email: i@qzier.com
 ;; content: tex && latex
 
+(require 'tex-mik)
 
-; XeLaTeX
+(setq TeX-auto-save t)
+(setq TeX-parse-self t)
+(setq-default TeX-master nil)
+
+(add-hook 'LaTeX-mode-hook 'visual-line-mode)
+(add-hook 'LaTeX-mode-hook 'flyspell-mode)
+(add-hook 'LaTeX-mode-hook 'LaTeX-math-mode) ;; math
+
+;; reftex
+(add-hook 'LaTeX-mode-hook 'turn-on-reftex)
+(setq reftex-plug-into-AUCTeX t)
+
+;; more default config
+(add-hook 'LaTeX-mode-hook
+	  (lambda ()
+	    (setq TeX-auto-untabify t     ; remove all tabs before saving
+		  TeX-engine 'xelatex       ; use xelatex default
+		  TeX-show-compilation t) ; display compilation windows
+	    (TeX-global-PDF-mode t)       ; PDF mode enable, not plain
+	    (setq TeX-save-query nil)
+	    (imenu-add-menubar-index)
+	    (define-key LaTeX-mode-map (kbd "TAB") 'TeX-complete-symbol)))
+
+
+;; XeLaTeX compiler
 (add-hook 'LaTeX-mode-hook 
 	  (lambda()
 	    (add-to-list 'TeX-command-list '("XeLaTeX" "%`xelatex%(mode)%' %t" TeX-run-TeX nil t))

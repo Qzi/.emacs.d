8ac3b059bfb01e87ecb39991942b64081abffd68
qzi <i@qzier.com>
2013-03-31 20:55:52 +0800

    vcs

---
+++ init-vcs.el
@@ -0,0 +1,9 @@
+;; author: qzi
+;; email: i@qzier.com
+;; content: git
+
+ (eval-after-load "magit"
+   '(set-face-attribute 'magit-item-highlight nil :foreground "#ffffff" :background "#3f4747"))
+
+
+(provide 'init-vcs)

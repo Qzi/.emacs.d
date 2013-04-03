1babaed9d36ba430cedeeb04916c4a63d7a35a4d
qzi <i@qzier.com>
2013-04-04 02:33:40 +0800

    lisp 关联优化

--- init-lisp.el
+++ init-lisp.el
@@ -25,30 +25,56 @@
 (setq scheme-program-name "/opt/local/bin/scheme")
 
 
-;; Common lisp
-;; ============
-;;
-;; desc: quicklisp
-;; link: http://www.quicklisp.org
-;;
-(load (expand-file-name "~/quicklisp/slime-helper.el"))
-;; Replace "sbcl" with the path to your implementation
-(setq inferior-lisp-program "/opt/local/bin/ccl64")
-;;(add-to-list 'auto-mode-alist '("\\.lisp$" . lisp-mode)) 
+;;;; Common lisp
+;;;; ============
+;;;;
+;;;; desc: quicklisp
+;;;; link: http://www.quicklisp.org
+;;;;
+;;(load (expand-file-name "~/quicklisp/slime-helper.el"))
+;;;; Replace "sbcl" with the path to your implementation
+;;(setq inferior-lisp-program "/opt/local/bin/ccl64")
+;;;;(add-to-list 'auto-mode-alist '("\\.lisp$" . lisp-mode)) 
+;; 
+;; 
+;;;; desc: lisp-mode 自动完成功能
+;;;; deps: slime(quicklisp/slime-helper)
+;;;;
+;;(defun lisp-indent-or-complete (&optional arg)
+;;  (interactive "p")
+;;  (if (or (looking-back "^\\s-*") (bolp))
+;;      (call-interactively 'lisp-indent-line)
+;;      (call-interactively 'slime-indent-and-complete-symbol)))
+;; 
+;;;;(eval-after-load "lisp-mode"
+;;;;  '(progn
+;;;;     (define-key lisp-mode-map (kbd "TAB") 'lisp-indent-or-complete)))
+;;;;
 
+(add-hook 'lisp-mode-hook 
 
-;; desc: lisp-mode 自动完成功能
-;; deps: slime(quicklisp/slime-helper)
-;;
-(defun lisp-indent-or-complete (&optional arg)
-  (interactive "p")
-  (if (or (looking-back "^\\s-*") (bolp))
-      (call-interactively 'lisp-indent-line)
-      (call-interactively 'slime-indent-and-complete-symbol)))
+	  (lambda()
+	    ;; load the slime-helper
+	    (load (expand-file-name "~/quicklisp/slime-helper.el"))
+	    ;; Replace "sbcl" with the path to your implementation
+	    (setq inferior-lisp-program "/opt/local/bin/ccl64")
+	    ;; postfix 
+	    (add-to-list 'auto-mode-alist '("\\.lisp$" . lisp-mode)) 
+	    ;; start slime
+	    (slime)
 
-;;(eval-after-load "lisp-mode"
-;;  '(progn
-;;     (define-key lisp-mode-map (kbd "TAB") 'lisp-indent-or-complete)))
-;;
+	    ;; desc: lisp-mode 自动完成功能
+	    ;; deps: slime(quicklisp/slime-helper)
+	    (defun lisp-indent-or-complete (&optional arg)
+	      (interactive "p")
+	      (if (or (looking-back "^\\s-*") (bolp))
+		  (call-interactively 'lisp-indent-line)
+		(call-interactively 'slime-indent-and-complete-symbol)))
+	    
+	    ;;(eval-after-load "lisp-mode"
+	    ;;  '(progn
+	    ;;     (define-key lisp-mode-map (kbd "TAB") 'lisp-indent-or-complete)))
+	    ))
+
 
 (provide 'init-lisp)

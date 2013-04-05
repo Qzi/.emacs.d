b1ede83dda26453bfff6ecb580d3713a9f40406d
qzi <i@qzier.com>
2013-04-06 03:06:14 +0800

    ediff

--- init-vcs.el
+++ init-vcs.el
@@ -5,5 +5,7 @@
  (eval-after-load "magit"
    '(set-face-attribute 'magit-item-highlight nil :foreground "#ffffff" :background "#3f4747"))
 
+;; ediff
+(setq ediff-split-window-function 'split-window-horizontally)
 
 (provide 'init-vcs)
